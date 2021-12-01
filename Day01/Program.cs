a4206d139b8929c0e50f0a9c95ec1f29db492224
Szel <Szelzz@users.noreply.github.com>
2021-12-01 14:39:34 +0100

    Day 1 Part 1

---
+++ Day01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+var last = 0;
+var increaseCount = -1;
+foreach (var line in File.ReadLines("input.txt"))
+{
+    var current = int.Parse(line);
+    if (current > last)
+        increaseCount++;
+
+    last = current;
+}
+
+System.Console.WriteLine(increaseCount);
