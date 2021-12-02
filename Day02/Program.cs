7ddbde94a5e9cd1d4c9c22de1efd910bd86af494
Szel <Szelzz@users.noreply.github.com>
2021-12-03 00:10:41 +0100

    Format code

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -3,16 +3,21 @@ var depth = 0;
 foreach (var line in File.ReadLines("input.txt"))
 {
     var number = int.Parse(line.Split(" ")[1]);
-    if(line.StartsWith("forward")){
-        horizontal+= number;
-    }else if(line.StartsWith("down")){
+    if (line.StartsWith("forward"))
+    {
+        horizontal += number;
+    }
+    else if (line.StartsWith("down"))
+    {
         depth += number;
-    }else if(line.StartsWith("up")){
+    }
+    else if (line.StartsWith("up"))
+    {
         depth -= number;
     }
 }
 
-Console.WriteLine("Part 1: {0}", depth*horizontal);
+Console.WriteLine("Part 1: {0}", depth * horizontal);
 
 // Part Two
 
@@ -22,13 +27,18 @@ var aim = 0;
 foreach (var line in File.ReadLines("input.txt"))
 {
     var number = int.Parse(line.Split(" ")[1]);
-    if(line.StartsWith("forward")){
-        horizontal+= number;
+    if (line.StartsWith("forward"))
+    {
+        horizontal += number;
         depth += aim * number;
-    }else if(line.StartsWith("down")){
+    }
+    else if (line.StartsWith("down"))
+    {
         aim += number;
-    }else if(line.StartsWith("up")){
+    }
+    else if (line.StartsWith("up"))
+    {
         aim -= number;
     }
 }
-Console.WriteLine("Part 2: {0}", depth*horizontal);
+Console.WriteLine("Part 2: {0}", depth * horizontal);
