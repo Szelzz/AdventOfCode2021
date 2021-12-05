09682b2e264b7d382c0d69a6f1a42c0287a37b97
Szel <Szelzz@users.noreply.github.com>
2021-12-05 16:37:34 +0100

    Day 4 Part 1

---
+++ Day04/Board.cs
@@ -0,0 +1,49 @@
+class Board
+{
+    public int[,] grid = new int[5, 5];
+    private int[] rowScore = new int[5];
+    private int[] colScore = new int[5];
+
+    public Board(int[,] grid)
+    {
+        this.grid = grid;
+    }
+
+    public bool NextNumber(int number)
+    {
+        for (int r = 0; r < 5; r++)
+        {
+            for (int c = 0; c < 5; c++)
+            {
+                if (grid[r, c] == number)
+                {
+                    rowScore[r]++;
+                    colScore[c]++;
+                    grid[r, c] = -1; // mark number
+                    if (rowScore[r] == 5 || colScore[c] == 5)
+                    {
+                        // BINGO
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public int CalculateSum()
+    {
+        var sum = 0;
+        for (int r = 0; r < 5; r++)
+        {
+            for (int c = 0; c < 5; c++)
+            {
+                if (grid[r, c] != -1)
+                    sum += grid[r, c];
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,36 @@
+﻿var firstLine = File.ReadLines("input.txt").First();
+var numbers = firstLine.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+
+var boards = new List<Board>();
+var currentGrid = new int[5, 5];
+var currentRow = 0;
+foreach (var line in File.ReadLines("input.txt").Skip(2))
+{
+    if (line == "")
+        continue;
+
+    var rowNumbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+    for (int colNumber = 0; colNumber < 5; colNumber++)
+    {
+        currentGrid[currentRow, colNumber] = rowNumbers[colNumber];
+    }
+    currentRow++;
+    if (currentRow == 5)
+    {
+        var b = new Board(currentGrid);
+        boards.Add(b);
+        currentGrid = new int[5, 5];
+        currentRow = 0;
+    }
+}
+
+foreach (var number in numbers)
+{
+    foreach (var board in boards)
+    {
+        if(board.NextNumber(number)){
+            System.Console.WriteLine("Part 1: {0}", board.CalculateSum() * number);
+            return;
+        }
+    }
+}
