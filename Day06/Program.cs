91c150332cab6fd465be5a2ea4fe2ce7b5fd623d
Szel <Szelzz@users.noreply.github.com>
2021-12-07 21:38:03 +0100

    Day 6 part 2

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,12 +1,12 @@
-﻿var fish = File.ReadAllText("input.txt")
+﻿using System.Numerics;
+
+var fish = File.ReadAllText("input.txt")
     .Split(',')
     .Select(n => new LanternFish(int.Parse(n)))
     .ToList();
 
-
 for (int i = 0; i < 80; i++)
 {
-    Console.WriteLine("Day {0}, count {1}",i,fish.Count);
     foreach (var oneFish in fish.ToList())
     {
         if (oneFish.NextDay())
@@ -15,3 +15,33 @@ for (int i = 0; i < 80; i++)
 }
 
 Console.WriteLine("Part 1: {0}", fish.Count);
+
+
+var fishCount = new BigInteger[9];
+var fishCounters = File.ReadAllText("input.txt")
+    .Split(',')
+    .Select(n => int.Parse(n))
+    .ToList();
+
+for (int i = 0; i < 9; i++)
+    fishCount[i] = new BigInteger(fishCounters.Count(c => c == i));
+
+for (int day = 0; day < 256; day++)
+{
+    var fishCountNext = new BigInteger[9];
+    for (int i = 1; i < fishCount.Length; i++)
+    {
+        fishCountNext[i - 1] = fishCount[i];
+    }
+    fishCountNext[6] = fishCountNext[6] + fishCount[0];
+    fishCountNext[8] = fishCount[0];
+
+    fishCount = fishCountNext;
+}
+
+BigInteger sum = new BigInteger();
+for (int i = 0; i < fishCount.Length; i++)
+{
+    sum += fishCount[i];
+}
+Console.WriteLine("Part 2: {0}", sum);
