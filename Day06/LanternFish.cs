5b3abc563790c0f1469a6814c0f1826460638814
Szel <Szelzz@users.noreply.github.com>
2021-12-07 20:37:41 +0100

    Day 6 part 1

---
+++ Day06/LanternFish.cs
@@ -0,0 +1,24 @@
+﻿public class LanternFish
+{
+    public int Counter { get; private set; }
+    public LanternFish(int counter)
+    {
+        Counter = counter;
+    }
+
+    public LanternFish()
+        : this(8)
+    {
+    }
+
+    public bool NextDay()
+    {
+        Counter--;
+        if (Counter == -1)
+        {
+            Counter = 6;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Day06/Program.cs
@@ -0,0 +1,17 @@
+﻿var fish = File.ReadAllText("input.txt")
+    .Split(',')
+    .Select(n => new LanternFish(int.Parse(n)))
+    .ToList();
+
+
+for (int i = 0; i < 80; i++)
+{
+    Console.WriteLine("Day {0}, count {1}",i,fish.Count);
+    foreach (var oneFish in fish.ToList())
+    {
+        if (oneFish.NextDay())
+            fish.Add(new LanternFish());
+    }
+}
+
+Console.WriteLine("Part 1: {0}", fish.Count);
