58cba2f0d9f188c4064f4c45aa4adf53da86beac
Szel <Szelzz@users.noreply.github.com>
2021-12-07 23:49:36 +0100

    Day 7

---
+++ Day07/Day07/Program.cs
@@ -0,0 +1,17 @@
+﻿var positions = File.ReadAllText("input.txt")
+    .Split(',')
+    .Select(int.Parse)
+    .ToList();
+
+positions.Sort();
+var closest = positions[positions.Count / 2];
+
+var fuelLost = positions.Select(p => Math.Abs(p - closest)).Sum();
+Console.WriteLine("Part 1: {0}", fuelLost);
+
+var closest2 = Math.Round(positions.Average(), MidpointRounding.ToZero);
+var fuelLost2 = positions.Select(p => Math.Abs(p - closest2)).
+    Select(d => (d * (d + 1)) / 2)
+    .Sum();
+
+Console.WriteLine("Part 2: {0}", fuelLost2);
