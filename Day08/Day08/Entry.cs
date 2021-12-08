35719fc0404268b62e92938720948ce31ccc5624
Szel <Szelzz@users.noreply.github.com>
2021-12-08 13:00:50 +0100

    Day 8 part 1

---
+++ Day08/Day08/Entry.cs
@@ -0,0 +1,28 @@
+﻿
+
+class Entry
+{
+    public List<int> Pattern { get; set; }
+        = new List<int>();
+
+    public List<int> Output { get; set; }
+        = new List<int>();
+
+    public Entry(string line)
+    {
+        var parts = line.Split(' ');
+        var outputSwitch = false;
+        foreach (var part in parts)
+        {
+            if (part == "|")
+            {
+                outputSwitch = true;
+                continue;
+            }
+            if (outputSwitch)
+                Output.Add(part.Length);
+            else
+                Pattern.Add(part.Length);
+        }
+    }
+}
---
+++ Day08/Day08/Program.cs
@@ -0,0 +1,23 @@
+﻿// 0 - 6 seg
+// 1 - 2 seg -
+// 2 - 5 seg
+// 3 - 5 seg
+// 4 - 4 seg - 
+// 5 - 5 seg
+// 6 - 6 seg
+// 7 - 3 seg - 
+// 8 - 7 seg -
+// 9 - 6 seg
+
+var entries = new List<Entry>();
+foreach (var line in File.ReadLines("input.txt"))
+{
+    entries.Add(new Entry(line));
+}
+
+var count1 = entries.Sum(
+    e => e.Output.Where(o => new[] { 2, 4, 3, 7 }.Contains(o)).Count());
+
+Console.WriteLine("Part 1: {0}", count1);
+
+
