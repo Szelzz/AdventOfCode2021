6c1b6e4642f9c24b08debe9db0bac65ef57113fa
Szel <Szelzz@users.noreply.github.com>
2021-12-13 13:46:40 +0100

    Day 10 part 2

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -1,10 +1,10 @@
 ﻿var closingBrackets = "]})>";
 var bracketMap = new Dictionary<char, char>()
 {
-    { ']', '[' },
-    { ')', '(' },
-    { '}', '{' },
-    { '>', '<' }
+    { '(', ')' },
+    { '[', ']' },
+    { '{', '}' },
+    { '<', '>' }
 };
 
 var invalidBrackets = new List<char>();
@@ -15,7 +15,7 @@ foreach (var line in File.ReadLines("input.txt"))
     {
         if (closingBrackets.Contains(bracket))
         {
-            if (brackets.Last() != bracketMap[bracket])
+            if (bracketMap[brackets.Last()] != bracket)
             {
                 invalidBrackets.Add(bracket);
                 break;
@@ -43,3 +43,60 @@ var sum = invalidBrackets.Select(BracketScoring).Sum();
 Console.WriteLine("Part 1: {0}", sum);
 
 // Part 2
+
+var missingBrackets = new List<string>();
+foreach (var line in File.ReadLines("input.txt"))
+{
+    var brackets = new List<char>();
+    var invalidLine = false;
+    foreach (var bracket in line)
+    {
+        if (closingBrackets.Contains(bracket))
+        {
+            if (bracketMap[brackets.Last()] != bracket)
+            {
+                invalidLine = true;
+                break;
+            }
+            brackets.RemoveAt(brackets.Count - 1);
+        }
+        else
+            brackets.Add(bracket);
+    }
+    if (invalidLine)
+        continue;
+
+    brackets.Reverse();
+    missingBrackets.Add(
+        new string(
+            brackets.Select(b => bracketMap[b])
+                .ToArray()));
+}
+
+long MissingBraketsScoring(string missingBrackets)
+{
+    long score = 0;
+    foreach (var bracket in missingBrackets)
+    {
+        checked
+        {
+            score *= 5;
+            score += bracket switch
+            {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => 0
+            };
+        }
+    }
+    return score;
+}
+
+var scores = missingBrackets.Select(MissingBraketsScoring)
+    .OrderBy(b => b)
+    .ToList();
+
+var score = scores[scores.Count / 2];
+Console.WriteLine("Part 2: {0}", score);
