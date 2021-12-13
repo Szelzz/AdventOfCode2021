99410baf818a9363a474da3059e68650cdf53e5a
Szel <Szelzz@users.noreply.github.com>
2021-12-13 20:03:46 +0100

    Day 12 part 1

---
+++ Day12/Cave.cs
@@ -0,0 +1,59 @@
+﻿class Cave
+{
+    public Cave(string name)
+    {
+        Name = name;
+        if (name == name.ToUpper())
+            IsBig = true;
+    }
+
+    public bool IsBig { get; }
+    public string Name { get; }
+    public bool IsEnd => Name == "end";
+    public bool IsStart => Name == "start";
+
+    public List<Cave> Connections { get; }
+        = new List<Cave>();
+
+    public void AddConnetion(Cave connection)
+    {
+        if (!Connections.Contains(connection))
+            Connections.Add(connection);
+
+        if (!connection.Connections.Contains(this))
+            connection.Connections.Add(this);
+    }
+
+    public List<List<Cave>>? FindEnd(List<Cave>? path = null)
+    {
+        if (path == null)
+            path = new List<Cave>();
+
+        // small caves cannot be visited twice
+        if (!IsBig && path.Contains(this))
+            return null;
+
+        path.Add(this);
+        var allPaths = new List<List<Cave>>();
+        if (IsEnd)
+        {
+            allPaths.Add(path);
+            return allPaths;
+        }
+
+        foreach (var connection in Connections)
+        {
+            var newPaths = connection.FindEnd(path.ToList());
+            if (newPaths != null)
+                allPaths.AddRange(newPaths.Where(p => p.Any(c => c.IsEnd)));
+        }
+
+        return allPaths;
+    }
+
+    public override string ToString()
+    {
+        return "Cave [" + Name + "]";
+    }
+}
+
--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var caves = new List<Cave>();
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    var split = line.Split('-');
+
+    var cave1 = caves.FirstOrDefault(c => c.Name == split[0]);
+    if (cave1 == null)
+    {
+        cave1 = new Cave(split[0]);
+        caves.Add(cave1);
+    }
+    var cave2 = caves.FirstOrDefault(c => c.Name == split[1]);
+    if (cave2 == null)
+    {
+        cave2 = new Cave(split[1]);
+        caves.Add(cave2);
+    }
+    cave1.AddConnetion(cave2);
+}
+
+var start = caves.First(c => c.IsStart);
+var end = caves.First(c => c.IsEnd);
+
+var allPaths = start.FindEnd() ?? throw new Exception();
+
+
+Console.WriteLine("Part 1: {0}", allPaths.Count);
