cc83f2c7118e39cf1cc1fdde8d4d2ff6ce555d80
Szel <Szelzz@users.noreply.github.com>
2021-12-13 14:29:04 +0100

    Day 11

---
+++ Day11/Day11/Dumbo.cs
@@ -0,0 +1,51 @@
+﻿
+class Dumbo
+{
+    public Dumbo(int energy, int x, int y)
+    {
+        Energy = energy;
+        this.X = x;
+        this.Y = y;
+    }
+
+    public int Energy { get; private set; }
+    public int FlashCount { get; private set; }
+
+    public List<Dumbo> Neighbors { get; }
+        = new List<Dumbo>();
+
+    public int X { get; }
+    public int Y { get; }
+
+    public void AddNeighbor(Dumbo dumbo)
+    {
+        if (Math.Abs(X - dumbo.X) <= 1 && Math.Abs(Y - dumbo.Y) <= 1)
+        {
+            Neighbors.Add(dumbo);
+            dumbo.Neighbors.Add(this);
+        }
+    }
+
+    public void AddEnergy()
+    {
+        if (Energy > 9)
+            return;
+
+        Energy++;
+        if (Energy == 10)
+        {
+            FlashCount++;
+            foreach (var neighbor in Neighbors)
+            {
+                neighbor.AddEnergy();
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        if (Energy > 9)
+            Energy = 0;
+    }
+}
+
---
+++ Day11/Day11/Program.cs
@@ -0,0 +1,46 @@
+﻿var y = 0;
+
+var octopuses = new List<Dumbo>();
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    for (int x = 0; x < 10; x++)
+    {
+        var newDumbo = new Dumbo(int.Parse(line[x].ToString()), x, y);
+        foreach (var octopus in octopuses)
+        {
+            octopus.AddNeighbor(newDumbo);
+        }
+        octopuses.Add(newDumbo);
+    }
+    y++;
+}
+
+for (int i = 0; i < 100; i++)
+{
+    foreach (var octopus in octopuses)
+        octopus.AddEnergy();
+
+    foreach (var octopus in octopuses)
+        octopus.Reset();
+}
+
+var flashCount = octopuses.Sum(x => x.FlashCount);
+Console.WriteLine("Part 1: {0}", flashCount);
+
+var step = 101; // steps from part 1
+while(true)
+{
+    foreach (var octopus in octopuses)
+        octopus.AddEnergy();
+
+    if (octopuses.All(o => o.Energy == 10))
+        break;
+
+    foreach (var octopus in octopuses)
+        octopus.Reset();
+
+    step++;
+}
+
+Console.WriteLine("Part 2: {0}", step);
