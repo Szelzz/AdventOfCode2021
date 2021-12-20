621ebbb79f28711c02d8b492f483e3d13db58fca
Szel <Szelzz@users.noreply.github.com>
2021-12-20 19:47:55 +0100

    Day 15 part 1

---
+++ Day15/DijkstraCoordinate.cs
@@ -0,0 +1,43 @@
+﻿record Coordinate(int X, int Y);
+
+class DijkstraCoordinate
+{
+    public DijkstraCoordinate(int x, int y, int risk)
+    {
+        X = x;
+        Y = y;
+        Risk = risk;
+        Coordinate = new Coordinate(x, y);
+    }
+
+    public DijkstraCoordinate(Coordinate coordinate, int risk)
+    {
+        X = coordinate.X;
+        Y = coordinate.Y;
+        Coordinate = coordinate;
+        Risk = risk;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+    public int Risk { get; }
+
+    public bool IsStart => X == 0 && Y == 0;
+    public bool IsEnd => X == 99 && Y == 99;
+    public bool Visited { get; set; } = false;
+    public long Distance { get; set; } = long.MaxValue;
+
+    public Coordinate Coordinate { get; }
+
+    public bool IsCoordinate(Coordinate coordinate)
+    {
+        return coordinate.X == X && coordinate.Y == Y;
+    }
+
+    internal void CalculateDistance(DijkstraCoordinate sourceCoordinate)
+    {
+        var newDistance = sourceCoordinate.Distance + Risk;
+        if (newDistance < Distance)
+            Distance = newDistance;
+    }
+}
---
+++ Day15/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+var coordinates = new Dictionary<Coordinate, DijkstraCoordinate>();
+var y = 0;
+foreach (var line in File.ReadLines("input.txt"))
+{
+    for (int x = 0; x < 100; x++)
+    {
+        var c = new Coordinate(x, y);
+        coordinates.Add(c, new DijkstraCoordinate(c, int.Parse(line[x].ToString())));
+    }
+    y++;
+}
+
+var start = coordinates[new Coordinate(0,0)];
+var end = coordinates[new Coordinate(99, 99)];
+start.Distance = 0;
+
+var current = start;
+var next = new List<DijkstraCoordinate>();
+while (true)
+{
+    var adjacent = Adjacent(current.Coordinate);
+
+    foreach (var neighbor in adjacent)
+    {
+        var coordinate = coordinates[neighbor];
+        if (coordinate.Visited)
+            continue;
+
+        coordinate.CalculateDistance(current);
+        if(!next.Contains(coordinate))
+            next.Add(coordinate);
+    }
+    current.Visited = true;
+    next.Remove(current);
+    if (current == end)
+        break;
+
+    current = next
+        .OrderBy(d => d.Distance)
+        .First(d => !d.Visited);
+}
+Console.WriteLine("Part 1: {0}", current.Distance);
+
+static List<Coordinate> Adjacent(Coordinate coordinate)
+{
+    var coordinates = new List<Coordinate>();
+    if (coordinate.X != 0)
+        coordinates.Add(new Coordinate(coordinate.X - 1, coordinate.Y));
+    if (coordinate.X != 99)
+        coordinates.Add(new Coordinate(coordinate.X + 1, coordinate.Y));
+    if (coordinate.Y != 0)
+        coordinates.Add(new Coordinate(coordinate.X, coordinate.Y - 1));
+    if (coordinate.Y != 99)
+        coordinates.Add(new Coordinate(coordinate.X, coordinate.Y + 1));
+
+    return coordinates;
+}
