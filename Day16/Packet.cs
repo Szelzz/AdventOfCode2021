abfa1010283e755b7e964f51eda7bf31e3306de1
Szel <Szelzz@users.noreply.github.com>
2021-12-22 19:44:44 +0100

    Day 16 part 2 WIP

--- Day16/Packet.cs
+++ Day16/Packet.cs
@@ -1,7 +1,47 @@
 ﻿class Packet
 {
-    public int Type { get; set; }
+    public PacketType Type { get; set; }
     public int Version { get; set; }
-    public int Value { get; set; }
+    public int SubpacketsSize { get; set; }
+    public long Value { get; set; }
+    public int Position { get; set; }
+    public int Size { get; set; }
+    public LengthType LengthType { get; set; }
+
+
+    public long Evaluate(Stack<long> numbersForOperation)
+    {
+        checked
+        {
+            return Type switch
+            {
+                PacketType.Sum => numbersForOperation.Sum(),
+                PacketType.Product => numbersForOperation.Aggregate((a, b) =>   a * b),
+                PacketType.Minimum => numbersForOperation.Min(),
+                PacketType.Maximum => numbersForOperation.Max(),
+                PacketType.GreaterThen => numbersForOperation.Pop() > numbersForOperation.Pop() ? 1 : 0,
+                PacketType.LessThen => numbersForOperation.Pop() < numbersForOperation.Pop() ? 1 : 0,
+                PacketType.EqualTo => numbersForOperation.Pop() == numbersForOperation.Pop() ? 1 : 0,
+                _ => throw new InvalidOperationException(),
+            };
+        }
+    }
 }
 
+enum LengthType
+{
+    Length,
+    Packet
+}
+
+enum PacketType
+{
+    Sum,
+    Product,
+    Minimum,
+    Maximum,
+    Number,
+    GreaterThen,
+    LessThen,
+    EqualTo
+}
--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 
 var messageHex = File.ReadAllText("input.txt");
-
+messageHex = "9C0141080250320F1802104A08";
 var messageBytes = new byte[messageHex.Length / 2];
 for (int i = 0; i < messageHex.Length; i += 2)
 {
@@ -20,36 +20,90 @@ while (position <= (message.Length - 8))
 
 Console.WriteLine("Part 1: {0}", allPackets.Sum(p => p.Version));
 
+// Part 2
+
+
+
+var operationStack = new Stack<Packet>();
+var numbers = new Stack<long>();
+var packetLength = 0;
+foreach (var packet in allPackets)
+{
+    packetLength += packet.Size;
+    if (packet.Type == PacketType.Number)
+    {
+        numbers.Push(packet.Value);
+        var lastOperation = operationStack.Peek();
+
+        if ((lastOperation.LengthType == LengthType.Packet
+                && lastOperation.SubpacketsSize <= numbers.Count)
+            || (lastOperation.LengthType == LengthType.Length
+                && lastOperation.SubpacketsSize <= packetLength - lastOperation.Position - lastOperation.Size))
+        {
+            var operation = operationStack.Pop();
+            var numbersForOperation = new Stack<long>();
+            var max = numbers.Count;
+            for (int i = 0; i < max; i++)
+                numbersForOperation.Push(numbers.Pop());
+
+            numbers.Push(operation.Evaluate(numbersForOperation));
+        }
+    }
+    else
+    {
+        operationStack.Push(packet);
+    }
+}
+
+if (numbers.Count != 1)
+    throw new Exception();
+
+Console.WriteLine("Part 2: {0}", numbers.Pop());
+
+
 static Packet ParsePacket(BitArray message, ref int position)
 {
+    var startPosition = position;
     var packetVersion = message.ToByte(position, 3);
     position += 3;
-    var packetType = message.ToByte(position, 3);
+    var packetType = (PacketType)message.ToByte(position, 3);
     position += 3;
-    if (packetType == 4)
+    if (packetType == PacketType.Number)
     {
+        var valueBytes = new List<byte>();
         for (int i = 0; ; i += 5)
         {
-            var prefix = message.GetTrueValue(position);
-            //var next = message.ToByte(position, 4
-            position += 5;
+            var isEnd = !message.GetTrueValue(position++);
+            valueBytes.Add(message.ToByte(position, 4));
 
-            if (!prefix)
+            position += 4;
+            if (isEnd)
                 break;
         }
+        long value = 0;
+        if (valueBytes.Count > 8)
+            throw new InvalidOperationException();
+        for (int i = 0; i < valueBytes.Count; i++)
+        {
+            value <<= 4;
+            value |= ((long)valueBytes[i]);
+        }
+
         return new Packet()
         {
             Type = packetType,
-            Version = packetVersion
+            Version = packetVersion,
+            Value = value,
+            Size = position - startPosition,
+            Position = startPosition
         };
     }
     else
     {
         // operator
-        var lengthType = message.GetTrueValue(position);
-        position++;
+        var lengthType = message.GetTrueValue(position++) ? LengthType.Packet : LengthType.Length;
         int length;
-        if (lengthType)
+        if (lengthType == LengthType.Packet)
         {
             //11 bits
             var byteArray = new byte[]
@@ -83,7 +137,11 @@ static Packet ParsePacket(BitArray message, ref int position)
         return new Packet()
         {
             Type = packetType,
-            Version = packetVersion
+            Version = packetVersion,
+            SubpacketsSize = length,
+            Size = position - startPosition,
+            Position = startPosition,
+            LengthType = lengthType
         };
     }
 }
