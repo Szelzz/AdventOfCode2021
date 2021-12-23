846db1883a8d198860e8f94b183c2f0893295e6b
Szel <Szelzz@users.noreply.github.com>
2021-12-23 16:16:01 +0100

    Day 17 part 2

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -13,14 +13,44 @@ var target = new Area(
 
 var maxYVelocity = -target.Y1 - 1;
 var maxHeight = 0;
-var currentYVelocity = maxYVelocity;
-while (currentYVelocity > 0)
-{
+for (int currentYVelocity = maxYVelocity; currentYVelocity > 0; currentYVelocity--)
     maxHeight += currentYVelocity;
-    currentYVelocity--;
-}
 
 Console.WriteLine("Part 1: {0}", maxHeight);
 
+// Part 2
+var maxSteps = maxYVelocity * 2 + 2;
+var maxXVelocity = target.X2;
+
+var hits2 = 0;
+for (int xVel = 1; xVel <= maxXVelocity; xVel++)
+{
+    for (int yVel = target.Y1; yVel <= maxYVelocity; yVel++)
+    {
+        var xPos = 0;
+        var yPos = 0;
+        var currentXVel = xVel;
+        var currentYVel = yVel;
+
+        for (int step = 1; step <= maxSteps; step++)
+        {
+            xPos += currentXVel;
+            yPos += currentYVel;
+
+            if (xPos >= target.X1 && xPos <= target.X2
+                && yPos >= target.Y1 && yPos <= target.Y2)
+            {
+                hits2++;
+                break;
+            }
+
+            currentYVel--;
+            if (currentXVel != 0)
+                currentXVel--;
+        }
+    }
+}
+
+Console.WriteLine("Part 2: {0}", hits2);
 
 record Area(int X1, int X2, int Y1, int Y2);
